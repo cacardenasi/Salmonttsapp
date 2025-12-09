2e2a47c4bd08e969b94a8578ead4323d91ce2096
cacardenasi <ca.cardenasi@duocuc.cl>
2025-12-08 21:30:25 -0300

    Update CentroCultivo.java

--- src/main/java/model/CentroCultivo.java
+++ src/main/java/model/CentroCultivo.java
@@ -1,4 +1,28 @@
 package model;
 
-public class CentroCultivo {
+public class CentroCultivo extends UnidadOperativa {
+
+    private double toneladasProduccion;
+
+    public CentroCultivo(String nombre, String comuna, double toneladasProduccion) {
+        super(nombre, comuna);
+        this.toneladasProduccion = toneladasProduccion;
+    }
+
+    @Override
+    public void mostrarInformacion() {
+        System.out.println("=== Centro de Cultivo ===");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Comuna: " + comuna);
+        System.out.println("Producción: " + toneladasProduccion + " toneladas");
+    }
+
+    @Override
+    public String toString() {
+        return "CentroCultivo{" +
+                "nombre='" + nombre + '\'' +
+                ", comuna='" + comuna + '\'' +
+                ", toneladasProduccion=" + toneladasProduccion +
+                '}';
+    }
 }

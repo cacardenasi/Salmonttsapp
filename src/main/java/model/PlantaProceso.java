58060b162631447e5f0f7ebe5d3a3944e9881bbd
cacardenasi <ca.cardenasi@duocuc.cl>
2025-12-08 21:31:28 -0300

    Update PlantaProceso.java

--- src/main/java/model/PlantaProceso.java
+++ src/main/java/model/PlantaProceso.java
@@ -1,4 +1,28 @@
 package model;
 
-public class PlantaProceso {
+public class PlantaProceso extends UnidadOperativa {
+
+    private int capacidadProceso;
+
+    public PlantaProceso(String nombre, String comuna, int capacidadProceso) {
+        super(nombre, comuna);
+        this.capacidadProceso = capacidadProceso;
+    }
+
+    @Override
+    public void mostrarInformacion() {
+        System.out.println("=== Planta de Proceso ===");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Comuna: " + comuna);
+        System.out.println("Capacidad: " + capacidadProceso + " toneladas/día");
+    }
+
+    @Override
+    public String toString() {
+        return "PlantaProceso{" +
+                "nombre='" + nombre + '\'' +
+                ", comuna='" + comuna + '\'' +
+                ", capacidadProceso=" + capacidadProceso +
+                '}';
+    }
 }

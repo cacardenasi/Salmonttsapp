4c1b4253ef8e25118bb3f21d14ec9b54b1597707
cacardenasi <ca.cardenasi@duocuc.cl>
2025-12-08 21:31:57 -0300

    Update UnidadOperativa.java

--- src/main/java/model/UnidadOperativa.java
+++ src/main/java/model/UnidadOperativa.java
@@ -1,4 +1,25 @@
 package model;
 
 public class UnidadOperativa {
+
+    protected String nombre;
+    protected String comuna;
+
+    public UnidadOperativa(String nombre, String comuna) {
+        this.nombre = nombre;
+        this.comuna = comuna;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Unidad Operativa: " + nombre);
+        System.out.println("Comuna: " + comuna);
+    }
+
+    @Override
+    public String toString() {
+        return "UnidadOperativa{" +
+                "nombre='" + nombre + '\'' +
+                ", comuna='" + comuna + '\'' +
+                '}';
+    }
 }
